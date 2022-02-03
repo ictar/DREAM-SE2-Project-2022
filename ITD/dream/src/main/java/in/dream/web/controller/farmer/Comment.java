e96246bac6d480f7347616325118831690b7ef68
Zhijun Hu <78056693+NinoShima@users.noreply.github.com>
2022-02-03 21:51:00 +0100

    update

--- ITD/dream/src/main/java/in/dream/web/controller/farmer/Comment.java
+++ ITD/dream/src/main/java/in/dream/web/controller/farmer/Comment.java
@@ -1,5 +1,7 @@
 package in.dream.web.controller.farmer;
 
+import in.dream.ejb.models.Farmer;
+import in.dream.ejb.models.Post;
 import in.dream.ejb.services.ForumService;
 import org.apache.commons.text.StringEscapeUtils;
 
@@ -10,36 +12,53 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.sql.Timestamp;
 
-@WebServlet(name = "farmerComment", urlPatterns = "/farmer/Comment")
+@WebServlet(name = "farmerPost", urlPatterns = "/farmer/comment/*")
 public class Comment extends HttpServlet {
     @EJB(name = "in.dream.ejb.services/ForumService")
     private ForumService forumService;
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // if the farmer is not logged in, redirect to the login
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String pathCtx = getServletContext().getContextPath();
         HttpSession session = request.getSession();
         if(session.isNew() || session.getAttribute("farmer") == null) {
-            response.sendRedirect(pathCtx+"/farmer/login.jsp");
+            response.sendRedirect(pathCtx+"/farmer/request.jsp");
             return;
         }
+        Farmer fm = (Farmer)session.getAttribute("farmer");
+        Post ps = (Post)session.getAttribute("post");
+        String path = "/farmer/comment.jsp";
 
+        request.setAttribute("user", fm.getName());
+        request.setAttribute("post", forumService.getPostByID());
+        request.setAttribute("commentList", forumService.getComment(ps.getPostid()));
+
+
+        request.getRequestDispatcher(path).forward(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
         String title, content;
         try {
+            title = StringEscapeUtils.escapeJava(request.getParameter("title"));
             content = StringEscapeUtils.escapeJava(request.getParameter("content"));
-            Timestamp posttime = new Timestamp(System.currentTimeMillis());
+            Timestamp commenttime = new Timestamp(System.currentTimeMillis());
 
-            if(content== null || content.isEmpty()) {
+            if(title== null || title.isEmpty()) {
                 throw new Exception("Required field is missing.");
             }
+            Farmer fm = (Farmer)session.getAttribute("farmer");
+            Post ps = (Post)session.getAttribute("post");
 
-            forumService.createComment(content,posttime);
-            response.sendRedirect(getServletContext().getContextPath() + "/farmer/comment.jsp");
+            forumService.createComment(fm.getFarmerid(), ps.getPostid(), content,commenttime);
+            response.sendRedirect(getServletContext().getContextPath() + "/farmer/post.jsp");
 
         } catch (Exception e) {
             request.setAttribute("errorMsgReg", e.getMessage());
-            request.getRequestDispatcher("/farmer/index.jsp").forward(request, response);
+            request.getRequestDispatcher("/farmer/post.jsp").forward(request, response);
         }
+
     }
+
 }
