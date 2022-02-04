ded1c1ec58260648f7c5fec6f1b1bbcbb99347ee
RUI <dazaokindle@163.com>
2022-02-04 17:50:12 +0100

    delete dailyplan case

--- ITD/design/data-dailyplan.sql
+++
@@ -1,8 +0,0 @@
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan1", '2022-01-01', "content", "0", "deviation", 1);
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2", "2022-01-02", "content", "1", "deviation", 2);
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan3", "2022-01-03", "content", "1", "deviation", 3);
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan4", "2022-01-04", "content", "0", "deviation", 4);
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan5", "2022-01-05", "content", "1", "deviation", 5);
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan6", "2022-01-06", "content", "0", "deviation", 6);
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan7", "2022-01-07", "content", "0", "deviation", 7);
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan8", "2022-01-08", "content", "1", "deviation", 8);
