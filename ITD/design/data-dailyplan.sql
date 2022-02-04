0d43cbeee5df87bce18ef62b1591f7c3066efd63
RUI <dazaokindle@163.com>
2022-02-04 11:57:14 +0100

    sql-dailyplan

---
+++ ITD/design/data-dailyplan.sql
@@ -0,0 +1,8 @@
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2211", "2022.1.1", "content", "0", "maria");
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2212", "2022.1.2", "content", "1", "luca");
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2213", "2022.1.3", "content", "1", "fiona");
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2214", "2022.1.4", "content", "0", "lorenzo");
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2215", "2022.1.5", "content", "1", "eva");
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2216", "2022.1.6", "content", "0", "paolo");
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2217", "2022.1.7", "content", "0", "davide");
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2218", "2022.1.8", "content", "1", "peyman");
