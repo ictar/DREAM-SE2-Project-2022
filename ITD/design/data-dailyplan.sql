bc347dd8116f3324c62c48b466840d6c2db8037f
RUI <dazaokindle@163.com>
2022-02-04 13:20:23 +0100

    Update data-dailyplan.sql

--- ITD/design/data-dailyplan.sql
+++ ITD/design/data-dailyplan.sql
@@ -1,8 +1,8 @@
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2211", "2022.1.1", "content", "0", "maria");
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2212", "2022.1.2", "content", "1", "luca");
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2213", "2022.1.3", "content", "1", "fiona");
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2214", "2022.1.4", "content", "0", "lorenzo");
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2215", "2022.1.5", "content", "1", "eva");
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2216", "2022.1.6", "content", "0", "paolo");
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2217", "2022.1.7", "content", "0", "davide");
-insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2218", "2022.1.8", "content", "1", "peyman");
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan1", '2022-01-01', "content", "0", "deviation", 1);
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan2", "2022-01-02", "content", "1", "deviation", 2);
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan3", "2022-01-03", "content", "1", "deviation", 3);
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan4", "2022-01-04", "content", "0", "deviation", 4);
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan5", "2022-01-05", "content", "1", "deviation", 5);
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan6", "2022-01-06", "content", "0", "deviation", 6);
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan7", "2022-01-07", "content", "0", "deviation", 7);
+insert into dailyplan(title, date, content, status, deviation, agronomist) values("Dailyplan8", "2022-01-08", "content", "1", "deviation", 8);
